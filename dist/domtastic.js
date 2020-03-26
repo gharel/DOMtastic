7eb1365f4e449d3b2b51b652009acd6ce3a43615
Guillaume Harel <guillaume.harel@skazy.nc>
2020-03-26 16:06:17 +1100

    Release 0.16.2

--- dist/domtastic.js
+++ dist/domtastic.js
@@ -66,7 +66,7 @@
 	 *     $.extend({a: 1}, {b: 2}, {a: 3}); // {a: 3, b: 2}
 	 */
 
-	var extend = function extend(target) {
+	var extend$1 = function extend(target) {
 	  for (var _len = arguments.length, sources = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
 	    sources[_key - 1] = arguments[_key];
 	  }
@@ -91,6 +91,35 @@
 	    return collection.indexOf(item) === index;
 	  });
 	};
+	/**
+	 * Check if touch device
+	 *
+	 * @return {Boolean}
+	 */
+
+	var isTouch = function isTouch() {
+	  return Boolean('ontouchstart' in window);
+	};
+	/**
+	 * Check if mobile with media queries
+	 *
+	 * @param width Width under that mobile is true
+	 * @return {Boolean}
+	 */
+
+	var isMobileByMQ = function isMobileByMQ(width) {
+	  return 'matchMedia' in window && window.matchMedia("(max-width:" + width + "px)").matches;
+	};
+
+	var util = /*#__PURE__*/Object.freeze({
+		win: win,
+		toArray: toArray,
+		each: each,
+		extend: extend$1,
+		uniq: uniq,
+		isTouch: isTouch,
+		isMobileByMQ: isMobileByMQ
+	});
 
 	/**
 	 * @module Selector
@@ -501,7 +530,7 @@
 	    DOMtastic.call(this, $.apply(void 0, arguments));
 	  };
 
-	  extend(BaseClass.prototype, api);
+	  extend$1(BaseClass.prototype, api);
 	  return BaseClass;
 	}
 
@@ -1802,12 +1831,12 @@
 	}
 
 	extend($$1, dom_contains, type);
-	extend(api, array, css$1, dom_attr, dom, dom_class, dom_data, dom_extra, dom_html, event, event_trigger, event_ready, selector_closest, selector_extra);
+	extend(api, array, css$1, dom_attr, dom, dom_class, dom_data, dom_extra, dom_html, event, event_trigger, event_ready, selector_closest, selector_extra, util);
 	$$1.fn = api; // Version
 
-	$$1.version = '0.16.1'; // Util
+	$$1.version = '0.16.2'; // Util
 
-	$$1.extend = extend; // Provide base class to extend from
+	$$1.extend = extend$1; // Provide base class to extend from
 
 	if (typeof BaseClass !== 'undefined') {
 	  $$1.BaseClass = BaseClass($$1.fn);
