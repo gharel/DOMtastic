6bd6a879bb9789bf28ef9b8770e72a71807bce4b
Guillaume Harel <guillaume.harel@skazy.nc>
2020-03-26 16:05:37 +1100

    fix(release): remove gh-pages

--- build/gh-pages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cat index.yml ../README.md > index.md
-rsync --recursive --delete ../dist/ dist
-rsync --recursive --delete ../test/ test
-rsync --recursive --delete ../doc/ doc
