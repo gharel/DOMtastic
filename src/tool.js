edbc0a9aa4a14e55e5138087359cbf1dc63d9f27
Guillaume Harel <guillaume.harel@skazy.nc>
2020-03-27 14:21:10 +1100

    feat(tool): add parseint function

--- src/tool.js
+++ src/tool.js
@@ -13,8 +13,17 @@ export const isTouch = () => Boolean('ontouchstart' in window);
 /**
  * Check if mobile with media queries
  *
- * @param width Width under that mobile is true
+ * @param {Number|String} [width] Width under that mobile is true
  * @return {Boolean}
  */
 
 export const isMobileByMQ = (width) => 'matchMedia' in window && window.matchMedia(`(max-width:${width}px)`).matches;
+
+/**
+ * Convert value into int correctly
+ *
+ * @param {Number|String} [value] Value under that mobile is true
+ * @return {Int}
+ */
+
+export const int = (value) => parseInt(value, 10);
